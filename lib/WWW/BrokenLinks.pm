49377fd0f4c6661d8b88d85483e3c03ef5e18c74
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-28 15:57:25 +0100

    Initial module code
    Should be enough here to run and test

--- lib/WWW/BrokenLinks.pm
+++ lib/WWW/BrokenLinks.pm
@@ -8,6 +8,9 @@ use warnings;
 use Moose;
 use namespace::autoclean;
 
+use WWW::Mechanize;
+use URI;
+
 our $VERSION = '0.01';
 
 has 'base_url' => (
@@ -16,9 +19,57 @@ has 'base_url' => (
   required => 1,
 );
 
+has 'debug' => (
+  is => 'ro',
+  isa => 'Bool',
+  required => 0,
+  default => false,
+);
+
 sub crawl
 {
+  my $self = shift;
+  my @crawl_queue = ();
+  my @broken_links = ();
+  my %scanned_urls = ();
   
+  my $mech = WWW::Mechanize->new();
+  my $current_url = $self->base_url;
+  $scanned_urls{$current_url} = 1;
+  
+  while ($current_url)
+  {
+    my $response = $mech->get($current_url);
+    my @links = $mech->links();
+    
+    for my $link (@links)
+    {
+      my $abs_url = URI->new_abs($link_url, $current_url)->canonical;
+      
+      $response = $mech->get($abs_url);
+      
+      if ($response->is_success)
+      {
+        if ($abs_url =~ m/$self->base_url/ && !exists($scanned_urls{$abs_url}))
+        {
+          # Local link which we haven't checked, so add to the crawl queue
+          push(@crawl_queue, $abs_url);
+        }
+        
+        # Always mark a successful URL as scanned, even if it is not local
+        $scanned_urls{$abs_url} = 1;
+      }
+      else
+      {
+        push(@broken_links, {'source' => $current_url, 'dest' => $abs_url});
+        print $current_url . ',' . $abs_url . "\n";
+      }
+    }
+    
+    sleep 1;
+    
+    $current_url = pop(@crawl_queue);
+  }
 }
 
 __PACKAGE__->meta->make_immutable;
